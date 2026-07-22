928a396ca2fcacb07ecc9355c67ee2428ded646b
Isha Negi <negi.isha98@gmail.com>
2026-07-22 18:16:30 +0530

    remove drizzle

--- drizzle/0000_blushing_leader.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "visitors" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"ip_hash" varchar(255) NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
--- drizzle/0001_wakeful_havok.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "visitors" ADD COLUMN "visited_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "visitors" DROP COLUMN "ip_hash";--> statement-breakpoint
-ALTER TABLE "visitors" DROP COLUMN "created_at";
